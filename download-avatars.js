a92f3a19884a5347a6fcc2f5ce1317e7dadc46e6
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-11 20:55:20 +0000

    step 5 make the request

--- download-avatars.js
+++ download-avatars.js
@@ -2,12 +2,25 @@ var request = require('request')
 
 console.log('Welcome to the GitHub Avatar Downloader!')
 
-var GITHUB_USER = "tikagan"
-var GITHUB_TOKEN = "4655bb968fd2bbee949d1985a1155e8c87b75bd6"
+const GITHUB_USER = "tikagan"
+const GITHUB_TOKEN = "4655bb968fd2bbee949d1985a1155e8c87b75bd6"
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  let requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
-  console.log('requestURL: ', requestURL)
+  let reqObj = {
+    url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': GITHUB_USER
+    }
+  }
+  request.get(reqObj, function(err, response, body){
+    if (err) {
+      console.log("Error: ", err)
+      return
+    }
+    console.log(body)
+    return
+  })
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
