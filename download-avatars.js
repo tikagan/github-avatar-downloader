bc397adf8acb9879b10c1ee21ee89985b76fde4d
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2017-09-11 18:24:58 +0000

    step 4: create the requestURS

--- download-avatars.js
+++ download-avatars.js
@@ -2,11 +2,15 @@ var request = require('request')
 
 console.log('Welcome to the GitHub Avatar Downloader!')
 
-function getRepoContributors(repoOwner, repoName, cb) {
+var GITHUB_USER = "tikagan"
+var GITHUB_TOKEN = "4655bb968fd2bbee949d1985a1155e8c87b75bd6"
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  let requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+  console.log('requestURL: ', requestURL)
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Errors:", err)
+  console.log("Result:", result)
 });
